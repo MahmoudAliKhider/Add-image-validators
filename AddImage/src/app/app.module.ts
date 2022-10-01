e14b907609c5d201a652e1f6934477ed246bc377
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-10-01 23:40:44 +0200

    create awork enviroment

--- AddImage/src/app/app.module.ts
+++ AddImage/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app.route.modul';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.model';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
