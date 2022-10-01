f7c5835e8e127f99fb65ec98ee9accd5ebd87a01
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-10-01 23:57:39 +0200

    Add image use base64

--- AddImage/src/app/app.component.ts
+++ AddImage/src/app/app.component.ts
@@ -9,8 +9,20 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+  images:any[]=[]
   title = 'AddImage';
+//base64
 
-  
+  getImage(event:any){
+  let files = event.target.files  //Array
+  for(let x=0;x<3;x++){
+    let filel  = files[x];
+    let reader = new FileReader()
+    reader.readAsDataURL(filel)
+    reader.onload = ()=>{
+   this.images.push(reader.result)
+    }
+  }
+  }
 }
 
