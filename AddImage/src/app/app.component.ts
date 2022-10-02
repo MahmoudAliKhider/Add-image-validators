0073dc40f98d316143f507ea2ee23a95b8e65d8a
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-10-02 13:25:19 +0200

    Scroll with images

--- AddImage/src/app/app.component.ts
+++ AddImage/src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent implements OnInit{
     throw new Error('Method not implemented.');
   }
   images:any[]=[];
-  preview :any=""
+  preview :any="";
+  ImageIndex!:number;
   title = 'AddImage';
 //base64
 
@@ -18,7 +19,7 @@ export class AppComponent implements OnInit{
 
   let files = event.target.files  //Array
   let sliderLength = this.images.length + files.length
-if(sliderLength>5){
+if(sliderLength > 5){
   let limit = 5 - this.images.length;
   for(let x=0;x<limit;x++){
     let filel  = files[x];
@@ -45,7 +46,33 @@ if(sliderLength>5){
 
   }
   display(index:any){
-this.preview =  this.images[index]
+    this.ImageIndex = index;
+    this.preview =  this.images[index]
+  }
+
+  Delate(){
+    this.images.splice(this.ImageIndex,1)
+    if(this.images.length == this.ImageIndex){
+      --this.ImageIndex;
+    this.preview =  this.images[this.ImageIndex]
+
+    }else{
+    this.preview =  this.images[this.ImageIndex]
+
+    }
+
+  }
+
+  minasImage(){
+
+    --this.ImageIndex;
+    this.preview =  this.images[this.ImageIndex]
+
+  }
+  plussImage(){
+    ++this.ImageIndex;
+    this.preview =  this.images[this.ImageIndex]
+
   }
 }
 
